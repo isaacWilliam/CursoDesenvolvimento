8c7321a317ea356d922fc234387f34c115a9d38f
isaacWilliam <izakew19@gmail.com>
2023-09-20 18:42:53 -0300

    Novo exemplo

---
+++ programacao/Planos.cs
@@ -0,0 +1,9 @@
+using System.Reflection.Metadata;
+
+public enum Planos 
+{
+    PadraoBarato =  50,
+    PremiumBarato = 70,
+    PadraoCaro = 70,
+    PremiumCaro = 100,
+}
--- programacao/Program.cs
+++ programacao/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+public class PlanosAssinatura
+{
+    public static void Main()
+    {
+        int idade;
+        float orcamento;
+        Boolean temDesconto = false;
+
+        Console.WriteLine("Digite sua idade");
+        idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite seu orçamento: ");
+        orcamento = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite se sim se possuir desconto");
+        string desconto = Console.ReadLine();
+
+        if (desconto == "sim")
+        {
+            temDesconto = true;
+        }
+        if (idade >= 18)
+        {
+            if(idade <= 30){
+                Console.WriteLine("Suas opções de planos São.\n Plano Padrão 50,00. Digite (0).\n Plano Premium 80,00. Digite (1)");
+                
+            }else{
+
+            }
+        }else
+        {
+            Console.WriteLine("Você é menor de idade. Não pode escolher um plano.");
+        }
+
+    }
+}
