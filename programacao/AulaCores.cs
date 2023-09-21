59099932746c33fbd704b9794efbd90303e28350
isaacWilliam <izakew19@gmail.com>
2023-09-21 20:56:40 -0300

    aula sobre enum com cores, ajuste no console.

---
+++ programacao/AulaCores.cs
@@ -0,0 +1,7 @@
+
+Console.WriteLine("Escolha uma cor: \n 0-Amarelo\n 1-Vermelho \n 2-Verde \n 3-Azul \n 4-Laranja \n 5-Roxo \n 6-Branco \n 7-Preto");
+int numeroDigitado = int.Parse(Console.ReadLine());
+
+Cores corSelecionada = (Cores)numeroDigitado;
+
+Console.WriteLine("A cor escolhida foi: " + corSelecionada);
