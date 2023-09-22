88e19604ed0fc122ec01a2c66d7d40f3b8fca03d
isaacWilliam <izakew19@gmail.com>
2023-09-21 22:05:18 -0300

    ajuste na aula de enum.

--- programacao/AulaCores.cs
+++ programacao/AulaCores.cs
@@ -1,7 +1,7 @@
 
-Console.WriteLine("Escolha uma cor: \n 0-Amarelo\n 1-Vermelho \n 2-Verde \n 3-Azul \n 4-Laranja \n 5-Roxo \n 6-Branco \n 7-Preto");
-int numeroDigitado = int.Parse(Console.ReadLine());
+// Console.WriteLine("Escolha uma cor: \n 0-Amarelo\n 1-Vermelho \n 2-Verde \n 3-Azul \n 4-Laranja \n 5-Roxo \n 6-Branco \n 7-Preto");
+// int numeroDigitado = int.Parse(Console.ReadLine());
 
-Cores corSelecionada = (Cores)numeroDigitado;
+// Cores corSelecionada = (Cores)numeroDigitado;
 
-Console.WriteLine("A cor escolhida foi: " + corSelecionada);
+// Console.WriteLine("A cor escolhida foi: " + corSelecionada);
--- programacao/Program.cs
+++ programacao/Program.cs
@@ -1,82 +1,86 @@
-﻿using System.Security.Cryptography.X509Certificates;
-public class PlanosAssinatura
+﻿try
 {
-    public static void Main()
+    int idade;
+    float orcamento, valorPLano = 0;
+    Boolean temDesconto = false;
+    string nomePLano = "";
+
+    Console.WriteLine(" Digite sua idade: \n");
+    idade = int.Parse(Console.ReadLine());
+
+    if (idade >= 18)
     {
-        try
+        Console.WriteLine(" Digite seu orçamento: \n");
+        orcamento = float.Parse(Console.ReadLine());
+        if (orcamento <= 100)
         {
-            int idade;
-            float orcamento, valorPLano = 0;
-            Planos planoEscolhido = 0;
-            Boolean temDesconto = false;
-
-            Console.WriteLine(" Digite sua idade: \n");
-            idade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(" Digite seu orçamento: \n");
-            orcamento = float.Parse(Console.ReadLine());
-
-            Console.WriteLine(" Digite 'sim' se você possuir desconto: \n");
-            string desconto = Console.ReadLine();
-            if (desconto == "sim")
+            if (orcamento >= 40)
             {
-                temDesconto = true;
-            }
-
-            if (idade >= 18)
-            {
-                int plano = 0;
-                if (idade <= 30)
-                {
-                    Console.WriteLine(" Suas opções de planos São.\n Plano Padrão 50,00. Digite (0).\n Plano Premium 80,00. Digite (1)");
-                    plano = int.Parse(Console.ReadLine());
-                    planoEscolhido = (Planos)plano;
-                    if (Planos.Padrao == planoEscolhido)
-                    {
-                        valorPLano = 50.00F;
-                    }
-                    else
-                    {
-                        valorPLano = 80.00F;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(" Suas opções de planos São.\n Plano Padrão 70,00. Digite (2).\n Plano Premium 100,00. Digite (3)");
-                    plano = int.Parse(Console.ReadLine());
-                    planoEscolhido = (Planos)plano;
-
-                    if (Planos.PadraoPlus == planoEscolhido)
-                    {
-                        valorPLano = 70.00F;
-                    }
-                    else
-                    {
-                        valorPLano = 100.00F;
-                    }
-
-                }
-
+                temDesconto = !temDesconto;
             }
             else
             {
-                Console.WriteLine(" Você é menor de idade. Não pode escolher um plano.");
-            }
-
-            if (temDesconto)
-            {
-                valorPLano = valorPLano - ((valorPLano * 20) / 100);
-                Console.WriteLine(" Seu Plano é o " + planoEscolhido + ", o valor é de " + valorPLano.ToString("C"));
-            }
-            else
-            {
-                Console.WriteLine(" Seu Plano é o " + planoEscolhido + ", o valor é de " + valorPLano.ToString("C"));
+                Console.WriteLine("Você não tem saldo o suficiente.");
+                return;
             }
         }
-        catch
+
+        int plano = 0;
+
+        Planos planoEscolhido = 0;
+        if (idade <= 30)
         {
+            Console.WriteLine(" Suas opções de planos São.\n Plano Padrão 50,00. Digite (0).\n Plano Premium 80,00. Digite (1)");
+            plano = int.Parse(Console.ReadLine());
+            planoEscolhido = (Planos)plano;
+            if (Planos.Padrao == planoEscolhido)
+            {
+                valorPLano = 50.00F;
+                nomePLano = "Padrão";
+            }
+            else
+            {
+                valorPLano = 80.00F;
+                nomePLano = "Premium";
+            }
+        }
+        else
+        {
+            Console.WriteLine(" Suas opções de planos São.\n Plano Padrão 70,00. Digite (2).\n Plano Premium 100,00. Digite (3)");
+            plano = int.Parse(Console.ReadLine());
+            planoEscolhido = (Planos)plano;
+
+            if (Planos.PadraoPlus == planoEscolhido)
+            {
+                valorPLano = 70.00F;
+                nomePLano = "Padrão";
+            }
+            else
+            {
+                valorPLano = 100.00F;
+                nomePLano = "Premium";
+            }
 
         }
 
     }
+    else
+    {
+        Console.WriteLine(" Você é menor de idade. Não pode escolher um plano.");
+        return;
+    }
+
+    if (temDesconto)
+    {
+        valorPLano = valorPLano - (valorPLano * 0.2F);
+        Console.WriteLine(" Seu Plano é o " + nomePLano + ", o valor é de " + valorPLano.ToString("C"));
+    }
+    else
+    {
+        Console.WriteLine(" Seu Plano é o " + nomePLano + ", o valor é de " + valorPLano.ToString("C"));
+    }
+}
+catch
+{
+
 }
